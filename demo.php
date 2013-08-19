4293dfc60e7041acb5da85075c87883b5a00442c
vitaly <vitaly@vitaly-KUbuntu>
2013-08-19 17:15:48 +0300

    reorganize demo.php

--- demo.php
+++ demo.php
@@ -21,98 +21,83 @@ test('getAvailabilityWithDetails');
 function test($mthName)
 {
     try{
-        if('getPropertyTypes' == $mthName) {
-            $res = Arn::getPropertyTypes( array('typeId'=>'1'),
-                                array()
-                                );
-            print_r($res);die;
-        }
+        switch ($mthName) {
+            case 'getPropertyTypes':
+                $res = Arn::getPropertyTypes( array('typeId'=>'1'),
+                                    array()
+                                    );
+                print_r($res);
+            break;
 
-        if('getPropertyAirports' == $mthName) {
-            $res = Arn::getPropertyAirports( array('propertyId'=>'42747', 'distance'=>5),
-                                array()
-                                );
-            print_r($res);die;
-        }
+            case 'getPropertyAirports':
+                $res = Arn::getPropertyAirports( array('propertyId'=>'42747', 'distance'=>5),
+                                    array()
+                                    );
+                print_r($res);
+            break;
 
-        if('getPOI' == $mthName) {
-            $res = Arn::getPOI( array(),
-                                array()
-                                );
-            print_r($res);die;
-        }
+            case 'getPOI':
+                $res = Arn::getPOI( array(),
+                                    array()
+                                    );
+                print_r($res);
+            break;
 
-        if('getMeetingRooms' == $mthName) {
-            $res = Arn::getMeetingRooms(  array('id'=>865, 'withOccupancy'=>0, 'meetingRoomOccupancyId'=>'4090'),
-                                        array());
-            print_r($res);die;
-        }
+            case 'getMeetingRooms':
+                $res = Arn::getMeetingRooms(  array('id'=>865, 'withOccupancy'=>0, 'meetingRoomOccupancyId'=>'4090'),
+                                            array());
+                print_r($res);
+            break;
 
-        if('getCountries' == $mthName) {
-            $res = Arn::getCountries(  array(),
-                                        array());
-            print_r($res);die;
-        }
+            case 'getCountries':
+                $res = Arn::getCountries(  array(),
+                                            array());
+                print_r($res);
+            break;
 
-        if('getContinents' == $mthName) {
-            $res = Arn::getContinents(  array(),
-                                        array());
-            print_r($res);die;
-        }
+            case 'getContinents':
+                $res = Arn::getContinents(  array(),
+                                            array());
+                print_r($res);
+            break;
 
-        if('getBrands' == $mthName) {
-            $res = Arn::getBrands(  array('code'=>array('A3','AA')),
-                                        false);
-            print_r($res);die;
-        }
+            case 'getBrands':
+                $res = Arn::getBrands(  array('code'=>array('A3','AA')),
+                                            false);
+                print_r($res);
+            break;
 
-        if('getAmenities' == $mthName) {
-            $res = Arn::getAmenities(  array('id'=>array('5','17'), 'typeId'=>8),
-                                        false);
-            print_r($res);die;
-        }
+            case 'getAmenities':
+                $res = Arn::getAmenities(  array('id'=>array('5','17'), 'typeId'=>8),
+                                            false);
+                print_r($res);
+            break;
 
-        if('getAmenityTypes' == $mthName) {
-            $res = Arn::getAmenityTypes(  array('id'=>array('1','2'), 'type'=>'123'),
-                                        false);
-            print_r($res);die;
-        }
+            case 'getAmenityTypes':
+                $res = Arn::getAmenityTypes(  array('id'=>array('1','2'), 'type'=>'123'),
+                                            false);
+                print_r($res);
+            break;
 
-        if('getAirports' == $mthName) {
-            $res = Arn::getAirports(  array('city'=>'chicago', 'airportCode'=>'MDW'),
-                                        array('city'),
-                                        20);
-            print_r($res);die;
-        }
+            case 'getAirports':
+                $res = Arn::getAirports(  array('city'=>'chicago', 'airportCode'=>'MDW'),
+                                            array('city'),
+                                            20);
+                print_r($res);
+            break;
 
-        if('getProperties' == $mthName) {
-            $res = Arn::getProperties(  array('city'=>'chicago', 'name'=>'hotel'),
-                                        array('city'),
-                                        20,
-                                        array('meeting_room', 'airport', 'amenity', 'descrition', 'image', 'policy', 'supplier')
-                    );
-            print_r($res);die;
-        }
+            case 'getProperties':
+                $res = Arn::getProperties(  array('city'=>'chicago', 'name'=>'hotel'),
+                                            array('city'),
+                                            20,
+                                            array('meeting_room', 'airport', 'amenity', 'descrition', 'image', 'policy', 'supplier')
+                        );
+                print_r($res);
+            break;
 
-        if('getAvailability' == $mthName) {
-            $res = Arn::getAvailability(    array(   
-                                                'inDate' => date('Y-m-d'), 
-                                                'outDate' => date('Y-m-d', strtotime('tomorrow')),
-                                                'rooms' => 1,
-                                                'adults' => 1,
-                                                'children' =>0,
-                                                'hotels'=>array('123')
-                                            ),
-                                            array('city'=>'chicago', 'name'=>'hotel'),
-                                            array('price'),
-                                            20
-                                        );
-            print_r($res);die;
-        }
-
-        if('getAvailabilityWithDetails' == $mthName) {
-            $res = Arn::getAvailabilityWithDetails(array(   
-                                                    'inDate' => date('Y-m-d'),
+            case 'getAvailability':
+                $res = Arn::getAvailability(    array(   
+                                                    'inDate' => date('Y-m-d'), 
                                                     'outDate' => date('Y-m-d', strtotime('tomorrow')),
                                                     'rooms' => 1,
                                                     'adults' => 1,
@@ -120,94 +105,111 @@ function test($mthName)
                                                     'hotels'=>array('123')
                                                 ),
                                                 array('city'=>'chicago', 'name'=>'hotel'),
-                                                array(),
-                                                10,
-                                                array('meeting_room', 'airport', 'amenity', 'descrition', 'image', 'policy', 'supplier')
-                                        );
-            print_r($res);die;
-        }
+                                                array('price'),
+                                                20
+                                            );
+                print_r($res);
+            break;
 
-        if ('getParticularRateDetails' == $mthName) {
-            $detailsData = Arn::getParticularRateDetails(array(   
-                                                    'inDate' => date('Y-m-d'), 
-                                                    'outDate' => date('Y-m-d', strtotime('tomorrow')),
-                                                    'rooms' => 1,
-                                                    'adults' => 1,
-                                                    'children' => 0,
-                                                    'hotelId' => 274659,
-                                                    'ratePlanCode' => '0RAC',
-                                                    'ratePlanGateway' => 4,
-                                                    'roomCode' => 'Z30'
-                                                    )
-                                                );
+            case 'getAvailabilityWithDetails':
+                $res = Arn::getAvailabilityWithDetails(array(   
+                                                        'inDate' => date('Y-m-d'),
+                                                        'outDate' => date('Y-m-d', strtotime('tomorrow')),
+                                                        'rooms' => 1,
+                                                        'adults' => 1,
+                                                        'children' =>0,
+                                                        'hotels'=>array('123')
+                                                    ),
+                                                    array('city'=>'chicago', 'name'=>'hotel'),
+                                                    array(),
+                                                    10,
+                                                    array('meeting_room', 'airport', 'amenity', 'descrition', 'image', 'policy', 'supplier')
+                                            );
+                print_r($res);
+            break;
 
-            $reservationDetails = Arn::prepareReservation($detailsData);
+            case 'getParticularRateDetails':
+                $detailsData = Arn::getParticularRateDetails(array(   
+                                                        'inDate' => date('Y-m-d'), 
+                                                        'outDate' => date('Y-m-d', strtotime('tomorrow')),
+                                                        'rooms' => 1,
+                                                        'adults' => 1,
+                                                        'children' => 0,
+                                                        'hotelId' => 274659,
+                                                        'ratePlanCode' => '0RAC',
+                                                        'ratePlanGateway' => 4,
+                                                        'roomCode' => 'Z30'
+                                                        )
+                                                    );
 
-            $userDetails = array(
-                'guestsPrimaryFirstName' => 'FN',
-                'guestsPrimaryMiddleName' => '',
-                'guestsPrimaryLastName' => 'LN',
-                'guestsPrimaryMessage' => '',
-                'guestsPrimaryEmail' => 'email@com.com',
-                'guestsPrimaryPhoneCountry' => '',
-                'guestsPrimaryPhoneArea' => '',
-                'guestsPrimaryPhoneNumber' => '123',
-                'guestsPrimaryPhoneExtension' => '',
-                'guestsPrimaryAgeGroup' => 'Senior',
-                'guestsAddressType' => 'Home',
-                'guestsAddressAddress' => 'adr',
-                'guestsAddressCity' => 'city',
-                'guestsAddressRegion' => '',
-                'guestsAddressPostalCode' => 'post',
-                'guestsAddressCountryCode' => 'US',
-                'guestsAddressExtraInfo' => '',
-                'ccType' => 'CA',
-                /*
-                 * You can use two fake card numbers to force credit card failures to ensure you handle them properly. 
-                 * 4321432143214327 will cause an authorization declined error and 5454545454545454 will cause an amount
-                 * that must be charged is different from amount authorized error. 4111111111111111 can be used on the
-                 * test gateways to book a successful reservation. 
-                 */
-                //'ccNumber' => '4321432143214327',
-                //'ccNumber' => '5454545454545454',
-                'ccNumber' => '4111111111111111',
-                'ccExpiration' => '12/15',
-                'ccCVV2' => '',
-                'ccHolder' => 'holder',
-                'ccAddress' => 'holder',
-                'ccCity' => 'holder',
-                'ccRegion' => '',
-                'ccPostalCode' => '',
-                'ccCountryCode' => 'US'
-            );
+                $reservationDetails = Arn::prepareReservation($detailsData);
 
-            $reservation = Arn::reservation($reservationDetails, $userDetails);
+                $userDetails = array(
+                    'guestsPrimaryFirstName' => 'FN',
+                    'guestsPrimaryMiddleName' => '',
+                    'guestsPrimaryLastName' => 'LN',
+                    'guestsPrimaryMessage' => '',
+                    'guestsPrimaryEmail' => 'email@com.com',
+                    'guestsPrimaryPhoneCountry' => '',
+                    'guestsPrimaryPhoneArea' => '',
+                    'guestsPrimaryPhoneNumber' => '123',
+                    'guestsPrimaryPhoneExtension' => '',
+                    'guestsPrimaryAgeGroup' => 'Senior',
+                    'guestsAddressType' => 'Home',
+                    'guestsAddressAddress' => 'adr',
+                    'guestsAddressCity' => 'city',
+                    'guestsAddressRegion' => '',
+                    'guestsAddressPostalCode' => 'post',
+                    'guestsAddressCountryCode' => 'US',
+                    'guestsAddressExtraInfo' => '',
+                    'ccType' => 'CA',
+                    /*
+                     * You can use two fake card numbers to force credit card failures to ensure you handle them properly. 
+                     * 4321432143214327 will cause an authorization declined error and 5454545454545454 will cause an amount
+                     * that must be charged is different from amount authorized error. 4111111111111111 can be used on the
+                     * test gateways to book a successful reservation. 
+                     */
+                    //'ccNumber' => '4321432143214327',
+                    //'ccNumber' => '5454545454545454',
+                    'ccNumber' => '4111111111111111',
+                    'ccExpiration' => '12/15',
+                    'ccCVV2' => '',
+                    'ccHolder' => 'holder',
+                    'ccAddress' => 'holder',
+                    'ccCity' => 'holder',
+                    'ccRegion' => '',
+                    'ccPostalCode' => '',
+                    'ccCountryCode' => 'US'
+                );
 
-            print_r($reservation);die;
-        }
+                $reservation = Arn::reservation($reservationDetails, $userDetails);
 
-        if('getByXML' == $mthName) {
-            $xml_string = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-    <ArnRequest>
-        <Availability DisplayCurrency=\"USD\" SearchTimeout=\"15\">
-            <HotelAvailability InDate=\"".date('Y-m-d')."\" OutDate=\"".date('Y-m-d', strtotime('tomorrow'))."\" Rooms=\"1\" Adults=\"2\" Children=\"0\">
-                <Hotel HotelID=\"274659\"/>
-            </HotelAvailability>
-        </Availability>
-    </ArnRequest>";
+                print_r($reservation);
+            break;
 
-            $result = Arn::getByXML($xml_string);
-            print_r($result);
-        }
+            case 'getByXML':
+                $xml_string = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+        <ArnRequest>
+            <Availability DisplayCurrency=\"USD\" SearchTimeout=\"15\">
+                <HotelAvailability InDate=\"".date('Y-m-d')."\" OutDate=\"".date('Y-m-d', strtotime('tomorrow'))."\" Rooms=\"1\" Adults=\"2\" Children=\"0\">
+                    <Hotel HotelID=\"274659\"/>
+                </HotelAvailability>
+            </Availability>
+        </ArnRequest>";
 
-        if ('reservationCancellation' == $mthName) {
-            $result = Arn::reservationCancellation(array(   
-                                                    'itineraryID' => '111',
-                                                    'reservationID' => '111'
-                                                    )
-                                                );
+                $result = Arn::getByXML($xml_string);
+                print_r($result);
+            break;
 
-            print_r($result);die;
+            case 'reservationCancellation':
+                $result = Arn::reservationCancellation(array(   
+                                                        'itineraryID' => '111',
+                                                        'reservationID' => '111'
+                                                        )
+                                                    );
+
+                print_r($result);
+            break;
         }
     }
     catch (Arn_Error $error) {
